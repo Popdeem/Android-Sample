7caa26cc5a6a24f189e597c62d88e09a887bc28f
Colm English <colm@Leos-iMac.local>
2018-08-10 16:36:28 +0100

    Updated to version 1.3.2 of the popdeem sdk

--- tabbedsample/src/main/java/com/popdeem/tabbedsample/MainActivity.java
+++ tabbedsample/src/main/java/com/popdeem/tabbedsample/MainActivity.java
@@ -128,10 +128,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode,resultCode,data);
-//    }
-
 
 }
